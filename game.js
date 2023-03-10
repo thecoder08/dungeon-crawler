ba6b480ff20cf9b8706d85c4c87f78148fb41c94
Lennon McLean <lmmclean08@gmail.com>
2023-03-10 13:27:55 -0500

    fixed issue

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
 var ctx = document.querySelector('canvas').getContext('2d');
+var player = {
+    x: 0,
+    y: 0,
+    width: 10,
+    height: 10
+};
 ctx.fillStyle = 'red';
 ctx.fillRect(player.x, player.y, player.width, player.height);
